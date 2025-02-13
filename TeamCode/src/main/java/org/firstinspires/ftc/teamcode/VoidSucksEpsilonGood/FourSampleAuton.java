cf8c0b2f8c7168edbff1f115d52cc0674dd77b0f
ImSuppressor <dylangibbs8550@gmail.com>
2025-02-13 10:50:52 -0800

    Update FourSampleAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
@@ -9,6 +9,7 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -24,10 +25,10 @@ public class FourSampleAuton extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
         Servo outtakearml = hardwareMap.servo.get("outtakearmL");
         Servo outtakearmr = hardwareMap.servo.get("outtakearmR");
-        Servo intakearm = hardwareMap.servo.get("IntakeArm");
-        Servo intakeclaw = hardwareMap.servo.get("IntakeClaw");
-        Servo intakeswivel = hardwareMap.servo.get("IntakeSwivel");
-        Servo clawrotate = hardwareMap.servo.get("ClawRotate");
+        Servo intakearm = hardwareMap.servo.get("intakeArm");
+        Servo intakeclaw = hardwareMap.servo.get("intakeClaw");
+        Servo intakeswivel = hardwareMap.servo.get("intakeSwivel");
+        Servo clawrotate = hardwareMap.servo.get("clawRotate");
         Servo outtakeclaw = hardwareMap.servo.get("outtakeClaw");
         Servo outtakeswivel = hardwareMap.servo.get("outtakeswivel");
         Servo linkr = hardwareMap.servo.get("linkR");
@@ -37,37 +38,32 @@ public class FourSampleAuton extends LinearOpMode {
 
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0, 0, 0))
-                        .strafeToLinearHeading(new Vector2d(0, 0), 0)
+                        .stopAndAdd(new Slidesruntoposition(15))
+                        .strafeToLinearHeading(new Vector2d(15, 15), 0)
+                        .waitSeconds(5)
                         .build());
     }
     public class Setpositionforservo implements Action {
         Servo servo;
         double position;
-
         public Setpositionforservo(Servo s, double p) {
             this.servo = s;
             this.position = p;
         }
-
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             servo.setPosition(position);
             return false;
         }
-
-
     }
-
     public class Setpositionforlink implements Action {
         Servo linkr = hardwareMap.servo.get("linkR");
         Servo linkl = hardwareMap.servo.get("LinkL");
         double InRobot;
         double position;
-
         public Setpositionforlink(double p) {
             this.position = p;
         }
-
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             if ((position) == InRobot) {
@@ -80,32 +76,29 @@ public class FourSampleAuton extends LinearOpMode {
                 linkl.setDirection(Servo.Direction.REVERSE);
                 linkr.setPosition(position);
                 linkl.setPosition(position);
-
                 return false;
             }
         }
-
+    }
         public class Slidesruntoposition implements Action {
             DcMotorEx SlideL = hardwareMap.get(DcMotorEx.class, "slideLeft");
             DcMotorEx SlideR = hardwareMap.get(DcMotorEx.class, "slideRight");
             double motorsetposition;
-
             public Slidesruntoposition(double sp) {
                 this.motorsetposition = sp;
             }
-
             @Override
             public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                 //find out which motor reversed
-                SlideR.setDirection(DcMotorSimple.Direction.REVERSE);
+                SlideL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                SlideR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                SlideL.setDirection(DcMotorSimple.Direction.REVERSE);
                 SlideR.setTargetPosition((int) motorsetposition);
                 SlideL.setTargetPosition((int) motorsetposition);
                 SlideR.setPower(1);
                 SlideL.setPower(1);
-
                 return false;
             }
         }
     }
 
-}
