069bdb2a57ce5dc05781d795cdd5cc002d8a0ac1
ImSuppressor <105533739+ImSuppressor@users.noreply.github.com>
2025-02-13 01:17:01 -0600

    Update FourSampleAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
@@ -9,14 +9,11 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
-
-import java.nio.channels.ClosedByInterruptException;
 
 @Autonomous(name="4SampleNoRizzleNorTizzle")
 public class FourSampleAuton extends LinearOpMode {
@@ -41,7 +38,6 @@ public class FourSampleAuton extends LinearOpMode {
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0, 0, 0))
                         .strafeToLinearHeading(new Vector2d(0, 0), 0)
-                        .stopAndAdd(new Setpositionforlink(linkl, linkr, 0))
                         .build());
     }
     public class Setpositionforservo implements Action {
@@ -63,39 +59,52 @@ public class FourSampleAuton extends LinearOpMode {
     }
 
     public class Setpositionforlink implements Action {
-        Servo servo0;
-        Servo servo1;
+        Servo linkr = hardwareMap.servo.get("linkR");
+        Servo linkl = hardwareMap.servo.get("LinkL");
+        double InRobot;
         double position;
 
-        public Setpositionforlink(Servo s0, Servo s1, double p) {
-            this.servo0 = s0;
-            this.servo1 = s1;
+        public Setpositionforlink(double p) {
             this.position = p;
         }
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            servo0.setPosition(position);
-            servo1.setPosition(position);
+            if ((position) == InRobot) {
+                linkl.setDirection(Servo.Direction.REVERSE);
+                linkr.setPosition(.12);
+                linkl.setPosition(.12);
+                return false;
+            } else {
+                //find out if linkl or linkr reversed and correct. Same with slides
+                linkl.setDirection(Servo.Direction.REVERSE);
+                linkr.setPosition(position);
+                linkl.setPosition(position);
 
-            return false;
-        }
-    }
-    public class Motorruntoposition implements Action {
-        DcMotor Lslide;
-        DcMotor Rslide;
-        double motorsetposition;
-        public Motorruntoposition(DcMotor Lslider, DcMotor Rslider, double sp) {
-            this.Lslide = Lslider;
-            this.Rslide = Rslider;
-            this.motorsetposition = sp;
+                return false;
+            }
         }
 
-        @Override
-        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            Lslide.setTargetPosition((int) motorsetposition);
-            Rslide.setTargetPosition((int) motorsetposition);
-            return false;
+        public class Slidesruntoposition implements Action {
+            DcMotorEx SlideL = hardwareMap.get(DcMotorEx.class, "slideLeft");
+            DcMotorEx SlideR = hardwareMap.get(DcMotorEx.class, "slideRight");
+            double motorsetposition;
+
+            public Slidesruntoposition(double sp) {
+                this.motorsetposition = sp;
+            }
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                //find out which motor reversed
+                SlideR.setDirection(DcMotorSimple.Direction.REVERSE);
+                SlideR.setTargetPosition((int) motorsetposition);
+                SlideL.setTargetPosition((int) motorsetposition);
+                SlideR.setPower(1);
+                SlideL.setPower(1);
+
+                return false;
+            }
         }
     }
 
