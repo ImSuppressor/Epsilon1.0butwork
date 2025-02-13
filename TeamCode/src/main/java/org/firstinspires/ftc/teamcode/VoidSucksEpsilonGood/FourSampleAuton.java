6d9c2663a12d7e650b7cfb2d3ae281a8a66039a3
ImSuppressor <dylangibbs8550@gmail.com>
2025-02-12 21:14:25 -0800

    Sigma auto
    oh yeah -kool-aid man said this y'know?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
@@ -1,12 +1,81 @@
 package org.firstinspires.ftc.teamcode.VoidSucksEpsilonGood;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "4SampleNoRizzleNorTizzle")
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+import java.nio.channels.ClosedByInterruptException;
+
+@Autonomous(name="4SampleNoRizzleNorTizzle")
 public class FourSampleAuton extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        Servo outtakearml = hardwareMap.servo.get("outtakearmL");
+        Servo outtakearmr = hardwareMap.servo.get("outtakearmR");
+        Servo intakearm = hardwareMap.servo.get("IntakeArm");
+        Servo intakeclaw = hardwareMap.servo.get("IntakeClaw");
+        Servo intakeswivel = hardwareMap.servo.get("IntakeSwivel");
+        Servo clawrotate = hardwareMap.servo.get("ClawRotate");
+        Servo outtakeclaw = hardwareMap.servo.get("outtakeClaw");
+        Servo outtakeswivel = hardwareMap.servo.get("outtakeswivel");
+        Servo linkr = hardwareMap.servo.get("linkR");
+        Servo linkl = hardwareMap.servo.get("LinkL");
 
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(0, 0, 0))
+                        .strafeToLinearHeading(new Vector2d(0, 0), 0)
+                        .stopAndAdd(new Setpositionforlink(linkl, linkr, 0))
+                        .stopAndAdd(new Setpositionforservo(Servo,position))
+                        .build());
+    }
+    public class Setpositionforservo implements Action {
+        Servo servo;
+        double position;
+
+        public Setpositionforservo(Servo s, double p) {
+            this.servo = s;
+            this.position = p;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            servo.setPosition(position);
+            return false;
+        }
+
+
+    }
+
+    public class Setpositionforlink implements Action {
+        Servo servo0;
+        Servo servo1;
+        double position;
+
+        public Setpositionforlink(Servo s0, Servo s1, double p) {
+            this.servo0 = s0;
+            this.servo1 = s1;
+            this.position = p;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            servo0.setPosition(position);
+            servo1.setPosition(position);
+
+            return false;
+        }
     }
 }
