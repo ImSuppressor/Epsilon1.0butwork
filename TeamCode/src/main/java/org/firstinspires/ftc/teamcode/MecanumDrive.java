6e23f086735bf6aaba65fd960bbdcd74417f877d
ImSuppressor <dylangibbs8550@gmail.com>
2025-02-14 15:08:42 -0800

    yeah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,18 +73,18 @@ public final class MecanumDrive {
         public double kA = 0.00018;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
+        public double maxWheelVel = 80;
         public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxProfileAccel = 80;
 
         // turn profile parameters (in radians)
         public double maxAngVel = 6; // shared with path
         public double maxAngAccel = 6;
 
         // path controller gains
-        public double axialGain = 1;
-        public double lateralGain = 1;
-        public double headingGain = 2; // shared with turn
+        public double axialGain = 3;
+        public double lateralGain = 3;
+        public double headingGain = 3; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/FourSampleAuton.java
@@ -40,7 +40,7 @@ public class FourSampleAuton extends LinearOpMode {
         Servo linkr = hardwareMap.servo.get("linkR");
         Servo linkl = hardwareMap.servo.get("linkL");
         //TODO:init actions for use during trajactories and/or parallel actions
-        IntakeClaw intakeClaw = new IntakeClaw(hardwareMap);
+        OuttakeClaw outtakeClaw = new OuttakeClaw(hardwareMap);
         //init
         outtakearmr.setDirection(Servo.Direction.REVERSE);
         linkr.setDirection(Servo.Direction.REVERSE);
@@ -56,17 +56,29 @@ public class FourSampleAuton extends LinearOpMode {
         clawrotate.setPosition(.5);
         //TODO:init paths
         Action sequence1 = drive.actionBuilder(new Pose2d(-64, -7, 0))//place first spec
-                .strafeToLinearHeading(new Vector2d(-27.5, -3), 0)
+                .stopAndAdd(new Slidesruntoposition(410))
+                .strafeToLinearHeading(new Vector2d(-28, -3), 0)
                 .build();
-        Action sequence2 = drive.actionBuilder(new Pose2d(-27.5, -3, 0))//move first sample
-                .strafeToLinearHeading(new Vector2d(-40, -30), -110)
-                .turn(-2.53)
+        Action sequence2 = drive.actionBuilder(new Pose2d(-28, -3, 0))//move first sample
+                .stopAndAdd(new Slidesruntoposition(0))
+                .stopAndAdd(new Intakeactions(.87, .45, .125, .5, .46))
+                .strafeToLinearHeading(new Vector2d(-40, -33), 109.85)
+                .turn(-2.48)
+                .stopAndAdd(new Intakeactions(.87, .45, .125, .5, .35))
                 .build();
-        Action sequence3 = drive.actionBuilder(new Pose2d(-27.5, -3, 0))//move second sample
-                .strafeToLinearHeading(new Vector2d(-50, -30), -110)
+        Action sequence3 = drive.actionBuilder(new Pose2d(-40, -33, 0))//move second sample
+                .strafeToLinearHeading(new Vector2d(-40, -41), 109.9)
+                .stopAndAdd(new Intakeactions(.87, .45, .125, .5, .46))
+                .waitSeconds(.25)
+                .turn(-2.55)
+                .stopAndAdd(new Intakeactions(.87, .45, .125, .5, .35))
                 .build();
-        Action sequence4 = drive.actionBuilder(new Pose2d(-27.5, -3, 0))//move third sample
-                .strafeToLinearHeading(new Vector2d(-50, -30), -110)
+        Action sequence4 = drive.actionBuilder(new Pose2d(-40, -41, 0))//move third sample
+                .stopAndAdd(new Intakeactions(.87, .45, .125, .5, .46))
+                .strafeToLinearHeading(new Vector2d(-40, -48), 109.9)
+                .waitSeconds(.25)
+                .turn(-3)
+                .stopAndAdd(new Intakeactions(.5, .45, .02, .5, .1))
                 .build();
         Action sequence5 = drive.actionBuilder(new Pose2d(-27.5, -3, 0))//move to pickup spec 2
                 .strafeToLinearHeading(new Vector2d(-50, -30), -110)
@@ -81,9 +93,12 @@ public class FourSampleAuton extends LinearOpMode {
         waitForStart();
 
 
-        Actions.runBlocking(new ParallelAction(
+        Actions.runBlocking(new SequentialAction(
                 sequence1,
-                intakeClaw.intakeclawopen()
+                outtakeClaw.outtakeclawopen(),
+                sequence2,
+                sequence3,
+                sequence4
         ));
     }
 
@@ -180,36 +195,81 @@ public class FourSampleAuton extends LinearOpMode {
             intakeclaw.setPosition(setpositionC);
             intakeswivel.setPosition(setpositionS);
             clawrotate.setPosition(setpositionR);
-            linkl.setDirection(Servo.Direction.REVERSE);
+            linkr.setDirection(Servo.Direction.REVERSE);
             linkr.setPosition(linkpos);
             linkl.setPosition(linkpos);
             return false;
         }
     }
-    public class IntakeClaw {
-        public Servo intakeclaw;
-        IntakeClaw(HardwareMap hardwareMap) {
-            intakeclaw = hardwareMap.get(Servo.class, ("intakeClaw"));
+    public class OuttakeClaw {
+        public Servo outtakeclaw;
+        OuttakeClaw(HardwareMap hardwareMap) {
+            outtakeclaw = hardwareMap.get(Servo.class, ("outtakeClaw"));
         }
-        public class intakeclawopen implements Action {
+        public class outtakeclawopen implements Action {
             @Override
             public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                intakeclaw.setPosition(.5);
+                outtakeclaw.setPosition(.5);
                 return false;
             }
         }
-        public class intakeclawclose implements Action {
+        public class outtakeclawclose implements Action {
             @Override
             public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                intakeclaw.setPosition(.4);
+                outtakeclaw.setPosition(.4);
                 return false;
             }
         }
-        public Action intakeclawopen(){
-            return new intakeclawopen();
+        public Action outtakeclawopen(){
+            return new outtakeclawopen();
         }
-        public Action intakeclawclose() {
-            return new intakeclawclose();
+        public Action outtakeclawclose() {
+            return new outtakeclawclose();
+        }
+    }
+    public class Slides {
+        public DcMotorEx SlideR;
+        public DcMotorEx SlideL;
+
+        Slides(HardwareMap hardwareMap) {
+            SlideR = hardwareMap.get(DcMotorEx.class, ("slideRight"));
+            SlideL = hardwareMap.get(DcMotorEx.class, ("slideLeft"));
+        }
+        public class slidesdown implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                SlideL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                SlideR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                SlideL.setDirection(DcMotorSimple.Direction.REVERSE);
+                SlideR.setTargetPosition((int) 0);
+                SlideL.setTargetPosition((int) 0);
+                SlideR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                SlideL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                SlideR.setPower(-1);
+                SlideL.setPower(-1);
+                return false;
+            }
+        }
+        public class slidesSpec implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                SlideL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                SlideR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                SlideL.setDirection(DcMotorSimple.Direction.REVERSE);
+                SlideR.setTargetPosition((int) 400);
+                SlideL.setTargetPosition((int) 400);
+                SlideR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                SlideL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                SlideR.setPower(1);
+                SlideL.setPower(1);
+                return false;
+            }
+        }
+        public Action slidesdown(){
+            return new slidesdown();
+        }
+        public Action slidesSpec() {
+            return new slidesSpec();
         }
     }
 }
