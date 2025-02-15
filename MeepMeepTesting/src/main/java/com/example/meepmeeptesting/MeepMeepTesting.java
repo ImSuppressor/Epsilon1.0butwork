62c96facce4d11aa896e07700f1825fce21607ba
ImSuppressor <105533739+ImSuppressor@users.noreply.github.com>
2025-02-15 15:33:54 -0600

    Added MeepMeeper

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,53 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 12)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-64, -7, 0))
+                        .strafeToLinearHeading(new Vector2d(-28, -0), 0)
+                        .waitSeconds(.5)
+                        .strafeToLinearHeading(new Vector2d(-40, -33), 109.85)
+                        .turn(-2.48)
+                        .strafeToLinearHeading(new Vector2d(-40, -41), 109.9)
+                        .turn(-2.55)
+                        .strafeToLinearHeading(new Vector2d(-40, -48), 109.9)
+                        .turn(-3)
+                        .strafeToLinearHeading(new Vector2d(-64, -25), 0)
+                        .waitSeconds(.5)
+                        .strafeToLinearHeading(new Vector2d(-28, -2), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-64, -25), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-28, -2), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-64, -25), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-28, -2), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-64, -25), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-28, -2), 0)
+                .waitSeconds(.5)
+                .strafeToLinearHeading(new Vector2d(-64, -25), 0)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
