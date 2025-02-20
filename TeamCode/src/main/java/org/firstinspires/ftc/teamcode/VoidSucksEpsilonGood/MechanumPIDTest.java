21204a8ab844e97d3f605e71a33467c063444f18
FreeGravity89 <ckab3411@gmail.com>
2025-02-20 16:36:04 -0600

    PID Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/MechanumPIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/MechanumPIDTest.java
@@ -18,17 +18,29 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.PIDController;
+import org.firstinspires.ftc.teamcode.ThreeDeadWheelLocalizer;
 
 @Autonomous(name="MechanumPIDTest")
 public class MechanumPIDTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotorEx SlideL = hardwareMap.get(DcMotorEx.class, "slideLeft");
-        DcMotorEx SlideR = hardwareMap.get(DcMotorEx.class, "slideRight");
-        DcMotorSimple leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        e leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        DcMotorEx leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        DcMotorEx leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         DcMotorEx rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
         DcMotorEx rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        // target in encoder ticks for each motor
+        double targetLF = 1000;
+        double targetLB = 1000;
+        double targetRB = 1000;
+        double targetRF = 1000;
+        //PID setup
+        PIDController leftFrontController = new PIDController(0.5,0.1,0.2);
+        PIDController leftBackController = new PIDController(0.5,0.1,0.2);
+        PIDController rightFrontController = new PIDController(0.5,0.1,0.2);
+        PIDController rightBackController = new PIDController(0.5,0.1,0.2);
+
+
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -37,10 +49,25 @@ public class MechanumPIDTest extends LinearOpMode {
 
         // TODO: reverse motor directions if needed
         //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-64, 42.5, 1.5708));
-        //TODO:init actions for use during trajactories and/or parallel actions
-        //TODO:init
+        if (targetLF > 0) {
+            //rightward strafe
+            leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+            rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        } else if (targetLF < 0) {
+            //leftward strafe
+            leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+            leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+            rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+            rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        //Set motor powers
+        leftFront.setPower(leftFrontController.output(targetLF,leftFront.getCurrentPosition()));
+        leftBack.setPower(leftBackController.output(targetLB,leftBack.getCurrentPosition()));
+        rightFront.setPower(rightFrontController.output(targetRF,rightFront.getCurrentPosition()));
+        rightBack.setPower(rightBackController.output(targetRB,rightBack.getCurrentPosition()));
+
     }
 }
