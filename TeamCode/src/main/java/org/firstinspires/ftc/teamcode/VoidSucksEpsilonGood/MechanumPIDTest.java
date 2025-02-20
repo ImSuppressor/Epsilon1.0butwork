b467f4cccf9f65ddf868a0f3521e2870f02af13f
FreeGravity89 <ckab3411@gmail.com>
2025-02-20 15:39:16 -0600

    Create MechanumPIDTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidSucksEpsilonGood/MechanumPIDTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.VoidSucksEpsilonGood;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous(name="MechanumPIDTest")
+public class MechanumPIDTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx SlideL = hardwareMap.get(DcMotorEx.class, "slideLeft");
+        DcMotorEx SlideR = hardwareMap.get(DcMotorEx.class, "slideRight");
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // TODO: reverse motor directions if needed
+        //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-64, 42.5, 1.5708));
+        //TODO:init actions for use during trajactories and/or parallel actions
+        //TODO:init
+    }
+}
